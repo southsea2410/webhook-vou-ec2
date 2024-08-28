a44585b6558c64c4b6305cee3b2c5500e51c8064
southsea2410 <ddhnam21@clc.fitus.edu.vn>
2024-08-28 21:39:14 +0700

    Add reload nginx command

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const { execSync } = require("child_process");
+const fs = require("fs");
 
 const app = express();
 const port = 3000; // Replace with your desired port
-const configFilePath = './vou.conf'; // Example path to Nginx config file
+const configFilePath = "/etc/nginx/conf.d/vou.conf"; // Example path to Nginx config file
+// const configFilePath = "./vou.conf"; // Example path to Nginx config file
 
 app.use(express.json());
 
-app.post('/update-ip', (req, res) => {
+app.post("/update-ip", (req, res) => {
   const newIP = req.body.ip;
 
   if (!newIP) {
-    return res.status(400).json({ error: 'IP address is required' });
+    return res.status(400).json({ error: "IP address is required" });
   }
-
 
   try {
     // Read the existing config file content
-    const configData = fs.readFileSync(configFilePath, 'utf8');
+    const configData = fs.readFileSync(configFilePath, "utf8");
 
     // Replace the old IP with the new one using regular expressions or string manipulation
-    const updated1ConfigData = configData.replace(/proxy_pass http:\/\/.+:/g, `proxy_pass http://[${newIP}]:`);
-    const updated2ConfigData = updated1ConfigData.replace(/proxy_pass https:\/\/.+;/g, `proxy_pass https://[${newIP}];`);
+    const updated1ConfigData = configData.replace(
+      /proxy_pass http:\/\/.+:/g,
+      `proxy_pass http://[${newIP}]:`
+    );
+    const updated2ConfigData = updated1ConfigData.replace(
+      /proxy_pass https:\/\/.+;/g,
+      `proxy_pass https://[${newIP}];`
+    );
 
     // Write the updated content back to the config file
-    fs.writeFileSync(configFilePath, updated2ConfigData, 'utf8');
+    fs.writeFileSync(configFilePath, updated2ConfigData, "utf8");
 
-    res.json({ message: 'IP address updated successfully' });
+    
+    // Optionally, you can reload the server here to apply the changes
+    execSync('nginx -s reload');
+    res.json({ message: "IP address updated successfully" });
   } catch (error) {
-    console.error('Error updating config file:', error);
-    res.status(500).json({ error: 'Failed to update config file' });
+    console.error("Error updating config file:", error);
+    res.status(500).json({ error: "Failed to update config file" });
+  }
+});
+
+app.get("/reload", (req, res) => {
+  try {
+    // Perform the reload operation using the appropriate command
+    // For example, if using Nginx, you can use the following command:
+    // execSync('nginx -s reload');
+
+    res.json({ message: "Server reloaded successfully" });
+  } catch (error) {
+    console.error("Error reloading server:", error);
+    res.status(500).json({ error: "Failed to reload server" });
   }
 });
 
